696cecd72ac1906b7593bdf6b1b1d6324e86547b
Thuy Nguyen <Thuy@Thuys-MacBook-Pro.local>
2016-02-16 13:31:59 -0600

    dom-maluplation hw

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 document.querySelector("#thanks button").addEventListener('click',function(){
-  // TASK #1
+  // TASK #1 //make an alert pop up 
 })
 
 document.querySelector("#double button").addEventListener('click',function(){
@@ -10,6 +10,17 @@ document.querySelector("#double button").addEventListener('click',function(){
 
 document.querySelector("#color-circle button").addEventListener('click',function(){
   // TASK #3
+
+  var circle = document.querySelector("#circle-bw")
+
+  if (circle.style.background === "black"){
+  	circle.style.background = "white"
+  }
+  else{
+  	circle.style.background = "black"
+  }
+
+
 })
 
 document.querySelector("#blow-up button").addEventListener('click',function(){
